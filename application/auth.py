9ea73b0aaf83efbe6fe1af6acc3291cd7997befa
EC2 Default User <ec2-user@ip-10-200-180-70.cn133.aws.cn>
2020-06-16 03:28:11 +0000

    add gitignore file

---
+++ application/auth.py
@@ -0,0 +1,34 @@
+from functools import wraps
+import requests
+import jwt
+import json
+from werkzeug.wrappers import Response
+
+def auth_failed(self, request , backend_svc, backend_svc_rpc_method):
+    response = Response("ITTZ Rocks!")
+    response.status = '401 Unauthorized'
+    return response
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(self, request , backend_svc, backend_svc_rpc_method ):
+        _authz_secret = '-----BEGIN PUBLIC KEY-----\n'+self.config.get('AUTHZ_SECRET')+'\n-----END PUBLIC KEY-----'
+        _authz_jwt_algorithms = self.config.get('AUTHZ_ALGORITHMS',['ES256'])
+        _request_encoded_jwt = request.headers.get('Authorization').split('Bearer ')[-1]
+        _request_payload = jwt.decode(_request_encoded_jwt, _authz_secret, algorithms=_authz_jwt_algorithms)
+        _authz_url = self.config.get('AUTHZ_URL')
+        _authz_payload = {
+            "principal": _request_payload['sub'],
+            "resource": "drn:aws:cn-north-1:oap2:"+str(backend_svc_rpc_method)+":*:*::"+str(backend_svc_rpc_method),
+        }
+        print(_authz_payload)
+        _authz_headers = {
+            "Content-Type":"application/json"
+        }
+        resp = requests.post(_authz_url, data=json.dumps(_authz_payload), verify=False, headers=_authz_headers)
+        print(resp.json()['data'])
+        if resp.json()['data']!=True:
+            return auth_failed(self, request , backend_svc, backend_svc_rpc_method)
+        return f(self, request , backend_svc, backend_svc_rpc_method)
+    return decorated
+    
